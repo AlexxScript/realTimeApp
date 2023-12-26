4cbb24348cf3584cdd93e7a3c4640dd4a725073c
alexamen9 <ameneyroalex@gmail.com>
2023-12-25 21:46:12 -0600

    app configuration

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,16 @@
-console.log("hello")
+import express from "express";
+import morgan from "morgan";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import dotenv from "dotenv/config";
+
+const app = express();
+const httpServer = new createServer(app);
+const io = new Server(httpServer);
+const port = process.env.PORT ?? 8081;
+
+app.use(morgan("tiny"));
+
+httpServer.listen(port,() => {
+    console.log(`Server running at port ${port}`);
+})
