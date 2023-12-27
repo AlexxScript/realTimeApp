f420bae8c70731220ae9cd8226c27ceab66a508b
alexamen9 <ameneyroalex@gmail.com>
2023-12-26 21:30:57 -0600

    adding controllers and models structure

--- server/src/index.js
+++ server/src/index.js
@@ -4,12 +4,16 @@ import { createServer } from "node:http";
 import { Server } from "socket.io";
 import dotenv from "dotenv/config";
 
+import homeRoute from "./routes/homeRoute.js";
+
 const app = express();
 const httpServer = new createServer(app);
 const io = new Server(httpServer);
 const port = process.env.PORT ?? 8081;
 
 app.use(morgan("tiny"));
+
+app.use("/",homeRoute);
 
 httpServer.listen(port,() => {
     console.log(`Server running at port ${port}`);
