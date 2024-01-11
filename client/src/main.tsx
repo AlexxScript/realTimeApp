fdd36c4895c2ccc8ca81d67296eaf930a182f1b7
alexamen9 <ameneyroalex@gmail.com>
2024-01-11 00:11:39 -0600

    Emit event react

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import * as ReactDom from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { App } from './App';
+import { Home } from './pages/Home';
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<App/>
+    },{
+        path:'/home',
+        element:<Home/>
+    }
+])
 
 const root = createRoot(document.getElementById('app')!);
 
-root.render(<App/>);
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
+);
