c7ab1e435ed1a9deb9cd5a39387e3c5131209074
alexamen9 <ameneyroalex@gmail.com>
2024-01-17 15:06:16 -0600

    Log in

--- client/src/pages/LogInUser.tsx
+++ client/src/pages/LogInUser.tsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const LoginUser = () => {
+    
+    const [fields,setFields] = useState({
+        userEmail:"",
+        password:""
+    });
+
+    const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setFields({
+            ...fields,
+            [e.target.name]:e.target.value
+        });
+    }
+
+    const handleSubmit = async (e:React.SyntheticEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            const res = await fetch('http://localhost:8080/user/login',{
+                method:'POST',
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify(fields)
+            });
+            const data = await res.json();
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
-        <form action="">
-            login
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Set your email</label>
+                <input onChange={handleChange} type="email" name="userEmail" />
+            </div>
+            <div>
+                <label>Set your password</label>
+                <input onChange={handleChange} type="password" name="password" />
+            </div>
+            <div>
+                <input type="submit" value="Log in" />
+            </div>
         </form>
     )
 }
