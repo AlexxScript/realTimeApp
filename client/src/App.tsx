a186939916081ecd044294a52836ac1865860ab6
alexamen9 <ameneyroalex@gmail.com>
2024-01-19 23:53:37 -0600

    solving redirect issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { socket } from "./socket/socket";
 import { AuthContext } from "./context/AuthContext";
 
@@ -6,25 +6,32 @@ export const App = () => {
 
     const contextAu = useContext(AuthContext);
 
+    const [messageW, setMessageW] = useState<{email:string}[]>([]);
+
     useEffect(() => {
         const room = contextAu.user.idSchool;
         const email = contextAu.user.email;
         if (contextAu.user.authenticated) {
-            socket.emit('joinRoomClient', { room,email });
+            socket.emit('joinRoomClient', { room, email });
         }
-
         socket.on('welcomeMessageServer', (data) => {
-            console.log(data);
+            setMessageW((prevMessage)=>[...prevMessage,data])
         })
 
         return () => {
             socket.off('joinRoomClient');
             socket.off('welcomeMessageServer');
         };
-    }, [socket,contextAu.user.idSchool,contextAu.user.email,contextAu.user.authenticated]);
+    }, [socket, contextAu.user.idSchool, contextAu.user.email, contextAu.user.authenticated]);
 
     if (contextAu.user.authenticated) {
-        return <h1>Hey! what's going on today? {contextAu.user.email}</h1>
+        return (
+            <div>
+                {messageW.map((message,index)=>(
+                    <h2 key={index}>hi {message.email}</h2>
+                ))}
+            </div>
+        )
     }
 
     return (
--- client/src/pages/MakeOrders.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const MakeOrders = () => {
-    return (
-        <h1>
-            make you order
-        </h1>
-    );
-}
