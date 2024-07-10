bddca0cd222cf78940d993bae61db89768c51764
alexamen9 <ameneyroalex@gmail.com>
2024-07-10 13:07:59 -0600

    a

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -31,6 +31,8 @@ export const NavBar = () => {
         }
     }
 
+    const handleButtonCart = ():void => {}
+
     if (!contextAu.user.role) {
         return (
             <nav className="navBarNav">
@@ -69,11 +71,18 @@ export const NavBar = () => {
 
     return (
         <nav className="navBarNav">
-            <button className="buttonBurger text-[#783016]" onClick={handleMenuButton}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="burguerButton bi bi-list" viewBox="0 0 16 16">
-                    <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
-                </svg>
-            </button>
+            <div className="flex flex-row justify-between">
+                <button className="buttonBurger text-[#783016]" onClick={handleMenuButton}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="burguerButton bi bi-list" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
+                    </svg>
+                </button>
+                <button className="mr-5">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-cart-fill" viewBox="0 0 16 16">
+                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+                    </svg>
+                </button>
+            </div>
 
             <ul className={`ulNavbar ${navState ? "active" : "inactive"}`}>
                 <li><Link className="linkNav" to="/profile">{contextAu.user.email}</Link></li>
@@ -81,6 +90,8 @@ export const NavBar = () => {
                 <li><Link className="linkNav" to="/">Orders</Link></li>
                 <li><button onClick={handleButtonLog}>Log out</button></li>
             </ul>
+
+
         </nav>
     )
 }
--- client/src/components/ShoppingCart.tsx
+++ client/src/components/ShoppingCart.tsx
@@ -67,20 +67,17 @@ export const ShoppingCart: React.FC<PropCart> = ({ dataItem, totalAcum }) => {
 
     return (
         // <div className="shoppingCart">
-        <div className={`bg-slate-800 bg-opacity-50 flex justify-center items-center top-0 right-0 bottom-0 left-0 flex-1 hidden`}>
+        <div className={`bg-slate-800 bg-opacity-50 flex justify-center items-center top-0 right-0 bottom-0 left-0 flex-1 fixed`}>
             <div className="flex h-64 justify-center">
                 <div className="relative ">
                     <div className="flex flex-row cursor-pointer truncate p-2 px-4  rounded">
-                        <div></div>
-                        <div className="flex flex-row-reverse ml-2 w-full">
-                            <div slot="icon" className="relative">
-                                {/* <div className="absolute text-xs rounded-full -mt-1 -mr-2 px-1 font-bold top-0 right-0 bg-red-700 text-white">3</div> */}
-                                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-shopping-cart w-6 h-6 mt-2">
-                                    <circle cx="9" cy="21" r="1"></circle>
-                                    <circle cx="20" cy="21" r="1"></circle>
-                                    <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+                        <div className="flex flex-row-reverse ml-2 w-full ">
+                            <button slot="icon" className="relative">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="32px" height="32px" fill="currentColor" className="bi bi-x-circle feather text-white feather-shopping-cart w-6 h-6 mt-2 hover:text-red-500 transition-all duration-150" viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
                                 </svg>
-                            </div>
+                            </button>
                         </div>
                     </div>
                     {dataItem.map((item, index) => (
@@ -110,9 +107,8 @@ export const ShoppingCart: React.FC<PropCart> = ({ dataItem, totalAcum }) => {
                                     </select>
                                 </div>
                                 <div className="text-gray-400">{item.available}</div>
-                                <button onClick={() => handleDelete(item.item_name)}>Delete</button>
                             </div>
-                            <div className="flex flex-col w-18 font-medium items-end">
+                            <button onClick={() => handleDelete(item.item_name)} className="flex flex-col w-18 font-medium items-end">
                                 <div className="w-4 h-4 mb-6 hover:bg-red-200 rounded-full cursor-pointer text-red-700">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-trash-2 ">
                                         <polyline points="3 6 5 6 21 6"></polyline>
@@ -121,7 +117,8 @@ export const ShoppingCart: React.FC<PropCart> = ({ dataItem, totalAcum }) => {
                                         <line x1="14" y1="11" x2="14" y2="17"></line>
                                     </svg>
                                 </div>
-                                ${item.qY * item.price}</div>
+                                ${item.qY * item.price}
+                            </button>
                         </div>
                     ))}
                     <div className="p-4 justify-center flex">
@@ -134,40 +131,6 @@ export const ShoppingCart: React.FC<PropCart> = ({ dataItem, totalAcum }) => {
                     </div>
                 </div>
             </div>
-            <button onClick={makeOrder}>Make order</button>
-            Total: {total}
-            {/* <div className="p-5"> */}
-            
-                        {/* <div className="absolute w-full  rounded-b border-t-0 z-10">
-                            <div className="shadow-xl w-64">
-                                <div className="p-2 flex bg-white hover:bg-gray-100 cursor-pointer border-b border-gray-100" >
-                                    <div className="p-2 w-12"></div>
-                                    <div className="flex-auto text-sm w-32">
-                                        <div className="font-bold">Product 1</div>
-                                        <div className="truncate">Product 1 description</div>
-                                        <div className="text-gray-400">Qt: 2</div>
-                                    </div>
-                                    <div className="flex flex-col w-18 font-medium items-end">
-                                        <div className="w-4 h-4 mb-6 hover:bg-red-200 rounded-full cursor-pointer text-red-700">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-trash-2 ">
-                                                <polyline points="3 6 5 6 21 6"></polyline>
-                                                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                                                <line x1="10" y1="11" x2="10" y2="17"></line>
-                                                <line x1="14" y1="11" x2="14" y2="17"></line>
-                                            </svg>
-                                        </div>
-                                        $12.22</div>
-                                </div>
-                                <div className="p-4 justify-center flex">
-                                    <button className="text-base  undefined  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer 
-                                    hover:bg-teal-700 hover:text-teal-100 
-                                    bg-teal-100 
-                                    text-teal-700 
-                                    border duration-200 ease-in-out 
-                                    border-teal-600 transition">Checkout $36.66</button>
-                                </div>
-                            </div>
-                        </div> */}
-                    </div>
+        </div>
     );
 };
