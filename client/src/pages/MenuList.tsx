1d648b49c122110642736b2cfda6c7c79fcb61ac
alexamen9 <ameneyroalex@gmail.com>
2024-07-12 15:40:13 -0600

    disenio de las muestras del menu

--- client/src/pages/MenuList.tsx
+++ client/src/pages/MenuList.tsx
@@ -104,31 +104,36 @@ export const MenuList = () => {
             {loading ? (
                 <p>Loading...</p>
             ) : (
-                <section className="mainContent">
-                    <h1>Menu</h1>
+                <section className="mainContent grid gap-8 md:grid-cols-2 lg:grid-cols-3">
                     {data.map((item, index) => (
-                        <div className="itemsList" key={index}>
-                            <p>{item.item_name + item.description + item.price + item.available + item.quantity}</p>
-                            <select
-                                name={item.item_name}
-                                value={cart.find((cartItem) => cartItem.item_name === item.item_name)?.qY || selectedQuantities[item.item_name]}
-                                onChange={(e) => handleChange(item.item_name, parseInt(e.target.value))}
-                            >
-                                {[...Array(item.quantity + 1).keys()].map((i) => (
-                                    <option key={i} value={i}>
-                                        {i}
-                                    </option>
-                                ))}
-                            </select>
-                            {
-                                item.available ? <button onClick={() => handleCart(item)}>add to cart</button> : <button disabled>add to cart</button>
-                            }
+                        <div className="itemsList relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md border border-blue-gray-100" key={index}>
+                            <div className="relative bg-clip-border mt-4 mx-4 rounded-xl overflow-hidden bg-transparent text-gray-700 shadow-none !m-0 p-6">
+                                <h5 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900 capitalize">{item.item_name}</h5>
+                                <p className="block antialiased font-sans text-sm leading-normal text-inherit font-normal !text-gray-500">{item.description}</p>
+                                <h3 className="antialiased tracking-normal font-sans text-xl font-semibold leading-snug text-blue-gray-900 flex gap-1 mt-4">Unitary price ${item.price}<span className="block antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-blue-gray-900 -translate-y-0.5 self-end opacity-70"></span></h3>
+                                { item.available ? <div>
+                                    <span className="mx-2">Select quantities</span>
+                                    <select
+                                        className="my-4"
+                                        name={item.item_name}
+                                        value={cart.find((cartItem) => cartItem.item_name === item.item_name)?.qY || selectedQuantities[item.item_name]}
+                                        onChange={(e) => handleChange(item.item_name, parseInt(e.target.value))}
+                                    >
+                                        {[...Array(item.quantity + 1).keys()].map((i) => (
+                                            <option key={i} value={i}>
+                                                {i}
+                                            </option>
+                                        ))}
+                                    </select>
+                                    {item.available}
+                                    <button  onClick={() => handleCart(item)} className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 px-4 rounded-lg border border-green-500 text-green-500 hover:opacity-75 focus:ring focus:ring-green-200 active:opacity-[0.85] block w-full mt-6" type="button">Add to cart</button>
+                                </div> : <h1 className="text-red-300">No available by moment</h1>}
+
+                            </div>
                         </div>
                     ))}
                 </section>
             )}
-            {/* <ShoppingCart dataItem={cart} totalAcum={total} /> */}
-            {/* <ShoppingCart /> */}
         </div>
     );
 }
